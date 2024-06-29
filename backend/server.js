8ab661e38534bce51d13274b9457c2f8f6db8607
Yuri Pierre-Louis <hazelirvin@gmail.com>
2024-06-29 15:18:00 -0400

    added remote origin

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const quizRoutes = require('./routes/quizRoute');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://technow.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
