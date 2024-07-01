cc98f917352e16623784752e7d9b13fb1b9bb07b
Yuri <hazelirvin@gmail.com>
2024-07-01 14:26:12 -0400

    cors update

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,7 +12,18 @@ const quizRoutes = require('./routes/quizRoute');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'https://tech-client-flax.vercel.app', credentials: true }));
+const allowedOrigins = ['https://tech-client-flax.vercel.app', 'https://tech-client-ookfck4j7-impact-team-5e808f93.vercel.app'];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps or curl requests)
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -28,7 +39,7 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB Connection
-const uri = 'mongodb+srv://yui561:Houbenove561%24@cluster0.c3jn9rd.mongodb.net/CompanyDB?retryWrites=true&w=majority&appName=Cluster0';
+const uri = process.env.MONGODB_URI || 'mongodb+srv://yui561:Houbenove561%24@cluster0.c3jn9rd.mongodb.net/CompanyDB?retryWrites=true&w=majority&appName=Cluster0';
 if (!uri) {
     console.error('MongoDB URI is not defined in the environment variables');
     process.exit(1); // Exit the application if URI is not defined
@@ -45,7 +56,7 @@ mongoose.connect(uri, {
     console.error('MongoDB connection error:', err);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
