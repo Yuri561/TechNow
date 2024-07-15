dc1819f899f0890141da3e4fe0882de11ad6b6b4
Houbenove (Yuri) Pierre-Louis <121592752+Yuri561@users.noreply.github.com>
2024-07-15 09:24:57 -0700

    Update server 4

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ const quizRoutes = require('./routes/quizRoute');
 const app = express();
 
 // Middleware
+const allowedOrigins = [
     'https://tech-now-plum.vercel.app/tool-box',
     'https://tech-now-plum.vercel.app/new-work-order-form',
     'https://tech-now-plum.vercel.app/docs-upload',
     'https://tech-now-plum.vercel.app/generate-receipt'
-   
 ];
 
 app.use(cors({
@@ -31,8 +31,6 @@ app.use(cors({
     },
     credentials: true
 }));
-
-
 
 // Set headers for all responses
 app.use((req, res, next) => {
@@ -49,9 +47,11 @@ app.use((req, res, next) => {
         next();
     }
 });
-app.options('*', cors())
+
+app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
+
 // Root Route
 app.get('/', (req, res) => {
     res.send({ message: 'hello world from server' });
@@ -59,12 +59,12 @@ app.get('/', (req, res) => {
 
 // Configure multer for file uploads
 const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, 'uploads/'); // Specify the directory to store the uploaded files
-	},
-	filename: function (req, file, cb) {
-		cb(null, `${Date.now()}-${file.originalname}`); // Use a unique name for the file
-	},
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/'); // Specify the directory to store the uploaded files
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`); // Use a unique name for the file
+    },
 });
 
 const upload = multer({ storage: storage });
@@ -77,40 +77,41 @@ app.use(quizRoutes);
 
 // Route for file upload
 app.post('/upload', upload.single('document'), (req, res) => {
-	if (req.file) {
-		res.status(200).json({
-			message: 'File uploaded successfully',
-			file: req.file,
-		});
-	} else {
-		res.status(400).json({
-			message: 'File not uploaded',
-		});
-	}
+    if (req.file) {
+        res.status(200).json({
+            message: 'File uploaded successfully',
+            file: req.file,
+        });
+    } else {
+        res.status(400).json({
+            message: 'File not uploaded',
+        });
+    }
 });
 
 // MongoDB Connection
 const uri = process.env.MONGODB_URI;
 if (!uri) {
-	console.error('MongoDB URI is not defined in the environment variables');
-	process.exit(1); // Exit the application if URI is not defined
+    console.error('MongoDB URI is not defined in the environment variables');
+    process.exit(1); // Exit the application if URI is not defined
 }
 
 mongoose
-	.connect(uri, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-	})
-	.then(() => {
-		console.log('Connected to MongoDB');
-	})
-	.catch((err) => {
-		console.error('MongoDB connection error:', err);
-	});
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
 
 const PORT = 5000;
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
 module.exports = app;
